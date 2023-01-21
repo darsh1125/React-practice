416a965dacf0e0baff6ebca3dcd914b6dacb76d3
darsh1125 <94056534+darsh1125@users.noreply.github.com>
2023-01-21 10:24:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <diV>
+    <h1>Practice</h1>
+    <ul>
+      <li>practice-1</li>
+      <li>practice-2</li>
+      <li>practice-3</li>
+    </ul>
+  </diV>,
+  document.getElementById("root")
+);
